546e7c074fd418d0068e460cbc0a063ee80e7839
Beatriz <beablancobejar@gmail.com>
2019-04-30 13:00:41 +0200

    segundo commit

--- src/validation/is-empty.js
+++ src/validation/is-empty.js
@@ -4,4 +4,6 @@ const isEmpty = value =>
     (typeof value === 'object' && Object.keys(value).length === 0) ||
     (typeof value === 'string' && value.trim().length === 0);
 
+
+
 export default isEmpty;
